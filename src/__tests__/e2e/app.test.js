d0376fec65c0b7573a861c96a7e4ce3690b0fea4
Ksenia Sidorova <nekfi@mail.ru>
2020-12-02 17:52:43 +0300

    Тесты

--- src/__tests__/e2e/app.test.js
+++ src/__tests__/e2e/app.test.js
@@ -1,18 +1,20 @@
 import puppeteer from 'puppeteer';
 
 describe('toggle', () => {
-    test('should remove class hidden for toggle', async() => {
-        let browser = null;
-        let page = null;
-         const baseUrl = 'http://localhost:9000';
-         browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 100,
-            devtools: true,
-        });
-        page = await browser.newPage();
-        await page.goto(baseUrl);
-        const btn = await page.$('[class=btn]');
-        btn.click();
+  test('should remove class hidden for toggle', async () => {
+    let browser = null;
+    let page = null;
+    const baseUrl = 'http://localhost:9000';
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
     });
+    page = await browser.newPage();
+    await page.goto(baseUrl);
+    const btn = await page.$('[class=btn]');
+    btn.click();
+    await page.waitForSelector('[class=popover]');
+    await browser.close();
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,24 @@
 const btn = document.getElementsByClassName('btn')[0];
 const div = document.getElementById('toggle');
-btn.onclick = function() {
-    if (document.getElementById('title')) {
-        const title = document.getElementById('title');
-        const text = document.getElementById('text');
-        title.remove();
-        text.remove();
-    } else {
-        const h1 = document.createElement('h1');
-        const p = document.createElement('p');
-        h1.id = 'title';
-        p.id = 'text';
-        h1.className = 'popover';
-        p.className ='popover';
-        h1.textContent = 'Popover title';
-        p.textContent = 'And here is some amazing content. It is very engaging. Right?';
-        div.insertAdjacentElement('afterbegin', p);
-        div.insertAdjacentElement('afterbegin', h1);
-        div.style.position = 'absolute';
-        div.style.left = '38%';
-        div.style.top = '19%';
-    }
-}
+btn.onclick = function () {
+  if (document.getElementById('title')) {
+    const title = document.getElementById('title');
+    const text = document.getElementById('text');
+    title.remove();
+    text.remove();
+  } else {
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+    h1.id = 'title';
+    p.id = 'text';
+    h1.className = 'popover';
+    p.className = 'popover';
+    h1.textContent = 'Popover title';
+    p.textContent = 'And here is some amazing content. It is very engaging. Right?';
+    div.insertAdjacentElement('afterbegin', p);
+    div.insertAdjacentElement('afterbegin', h1);
+    div.style.position = 'absolute';
+    div.style.left = '38%';
+    div.style.top = '19%';
+  }
+};
