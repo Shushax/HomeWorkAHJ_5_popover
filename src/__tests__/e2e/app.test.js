d25223ae75c20f29e30493da98af2c62ea6c3b2e
Ksenia Sidorova <nekfi@mail.ru>
2020-12-02 17:16:50 +0300

    Доработки

--- src/__tests__/e2e/app.test.js
+++ src/__tests__/e2e/app.test.js
@@ -1,25 +1,16 @@
 import puppeteer from 'puppeteer';
 
-jest.setTimeout(30000);
 describe('toggle', () => {
-    let browser = null;
-    let page = null;
-    const baseUrl = 'http://localhost:9000';
-    beforeAll(async() => {
-        browser = await puppeteer.launch({
+    test('should remove class hidden for toggle', async() => {
+        let browser = null;
+        let page = null;
+         const baseUrl = 'http://localhost:9000';
+         browser = await puppeteer.launch({
             headless: false,
             slowMo: 100,
             devtools: true,
         });
         page = await browser.newPage();
-    });
-    afterAll(async() => {
-        await browser.close();
-    });
-});
-
-describe('toggle', () => {
-    test('should remove class hidden for toggle', async() => {
         await page.goto(baseUrl);
         const btn = await page.$('[class=btn]');
         btn.click();
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,24 @@
 const btn = document.getElementsByClassName('btn')[0];
-const popover = document.getElementById('toggle');
+const div = document.getElementById('toggle');
 btn.onclick = function() {
-    popover.classList.toggle('hidden');
+    if (document.getElementById('title')) {
+        const title = document.getElementById('title');
+        const text = document.getElementById('text');
+        title.remove();
+        text.remove();
+    } else {
+        const h1 = document.createElement('h1');
+        const p = document.createElement('p');
+        h1.id = 'title';
+        p.id = 'text';
+        h1.className = 'popover';
+        p.className ='popover';
+        h1.textContent = 'Popover title';
+        p.textContent = 'And here is some amazing content. It is very engaging. Right?';
+        div.insertAdjacentElement('afterbegin', p);
+        div.insertAdjacentElement('afterbegin', h1);
+        div.style.position = 'absolute';
+        div.style.left = '38%';
+        div.style.top = '19%';
+    }
 }
